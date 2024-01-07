aae25fe7ef9f8dccf676d81cbfbbf3adefd39d36
ibrahim.sayar <ibrahim.sayar@naylalabs.com>
2024-01-07 14:00:02 +0300

    fix: change script dir

--- add-backup-time-to-crontab.sh
+++ add-backup-time-to-crontab.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Set variables
-SCRIPT_DIR="/home"
+SCRIPT_DIR="/home/linux-mongodb-aws-backup-script"
 CRONTAB_FILE="/var/spool/cron/crontabs/root"
 
 # Generate crontab command
-CRONTAB_COMMAND="0 5 * * * bash/$SCRIPT_DIR/mongodb-aws-backup-script.sh 1> /home/mongo-backups/log.txt 2> /home/mongo-backups/error.txt"
+CRONTAB_COMMAND="0 5 * * * bash/$SCRIPT_DIR/mongodb-aws-backup-script.sh 1> $SCRIPT_DIR/log.txt 2> $SCRIPT_DIR/error.txt"
 
 # Set crontab command to crontab
 echo "$CRONTAB_COMMAND" >> "$CRONTAB_FILE"
