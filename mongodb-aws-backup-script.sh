7fc4669a0f8d48bf5067eeb247cdb3c78d057b6a
ibrahim.sayar <ibrahim.sayar@naylalabs.com>
2024-01-07 13:54:49 +0300

    feat: bucket name defined to variable

---
+++ mongodb-aws-backup-script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# Set variables
+date=$(date '+%Y-%m-%d')
+backupDir=/home/mongo-backups/
+
+# Authenticate to MongoDB
+export MONGODB_USERNAME=mongodbDatabaseUser
+export MONGODB_PASSWORD=mongodbDatabasePassword
+export MONGODB_DATABASE=mongodbDatabase
+export S3_BUCKET_NAME=s3://bucket-name
+
+# Delete old backup files
+echo "Deleting old backup files..."
+find $backupDir -name "*.tar.gz" -type f -delete
+
+# Backup database
+echo "Backing up database..."
+mongodump --authenticationDatabase admin -u $MONGODB_USERNAME -p $MONGODB_PASSWORD --db $mongodbDatabase --out $backupDir$date
+
+# Compress backup directory
+echo "Compressing backup directory..."
+cd $backupDir
+tar -czvf $date.tar.gz $date/
+
+# Upload backup to S3
+echo "Uploading backup to S3..."
+aws s3 cp $backupDir/$date.tar.gz $S3_BUCKET_NAME/$date.tar.gz
+
+# Delete backup folder
+echo "Deleting backup folder"
+rm -rf $backupDir$date
